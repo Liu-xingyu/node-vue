1def57eb60007db906c41d6692a8168707044f88
YRP-Laptop <873750200@qq.com>
2018-10-31 21:35:57 +0800

    Initial commit

---
+++ src/components/Yerenpeng.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2><em>{{msg}}</em></h2>
+</template>
+
+<script>
+    export default {
+        name: 'Yerenpeng',
+        data(){
+              return{
+               msg: '这是自定义的一个Vue文件'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
